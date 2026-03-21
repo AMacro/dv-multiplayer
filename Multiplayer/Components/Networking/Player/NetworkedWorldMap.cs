32ffdf5b0987f85b571be636e593347a065a8d87
Macka <andrew@andrewcraigmackenzie.com>
2026-03-21 15:13:51 +1000

    Update MapMarkers to use the DisplayName
    Map markers will now show the crewname if available

--- Multiplayer/Components/Networking/Player/NetworkedWorldMap.cs
+++ Multiplayer/Components/Networking/Player/NetworkedWorldMap.cs
@@ -51,16 +51,16 @@ public class NetworkedMapMarkersController : MonoBehaviour
         GameObject textGo = Instantiate(textPrefab, root);
         textGo.transform.localPosition = new Vector3(0, 0.001f, 0);
         textGo.transform.localEulerAngles = new Vector3(90f, 0, 0);
-        refs.text = textGo.GetComponent<RectTransform>();
-        TMP_Text text = textGo.GetComponent<TMP_Text>();
+        refs.textRect = textGo.GetComponent<RectTransform>();
+        refs.text = textGo.GetComponent<TMP_Text>();
 
-        text.name = "Player Name";
-        text.text = player.Username;
-        text.alignment = TextAlignmentOptions.Center;
-        text.fontSize /= 1.25f;
-        text.fontSizeMin = text.fontSize / 2.0f;
-        text.fontSizeMax = text.fontSize;
-        text.enableAutoSizing = true;
+        refs.text.name = "Player Name";
+        refs.text.text = player.DisplayName;
+        refs.text.alignment = TextAlignmentOptions.Center;
+        refs.text.fontSize /= 1.25f;
+        refs.text.fontSizeMin = refs.text.fontSize / 2.0f;
+        refs.text.fontSizeMax = refs.text.fontSize;
+        refs.text.enableAutoSizing = true;
 
         playerIndicators[player] = refs;
     }
@@ -125,7 +125,7 @@ public class NetworkedMapMarkersController : MonoBehaviour
 
             Vector3 position = markersController.GetMapPosition(playerTransform.position - WorldMover.currentMove, true);
             refs.indicator.localPosition = position;
-            refs.text.localPosition = position with { y = position.y + 0.025f };
+            refs.textRect.localPosition = position with { y = position.y + 0.025f };
         }
     }
 }
