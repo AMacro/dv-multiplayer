8b25ab3690f87902586a1e8f2dd301e80618bad0
Macka <andrew@andrewcraigmackenzie.com>
2026-06-28 21:04:37 +1000

    Update models
    Fix prefab naming convention and create a new model for a Tic Tac for nostalgia with Patrick van Halm's original DV MP mod

--- MultiplayerAssets/Assets/Scripts/Multiplayer/AssetIndex.cs
+++ MultiplayerAssets/Assets/Scripts/Multiplayer/AssetIndex.cs
@@ -9,7 +9,8 @@ namespace Multiplayer.Editor
     {
         [Header("Prefabs")]
         public GameObject PlayerTag;
-        public GameObject[] playerPrefabs;
+        public GameObject defaultModel;
+        public GameObject[] modelPrefabs;
 
         [Header("Textures")]
         public Sprite multiplayerIcon;
@@ -17,45 +18,5 @@ namespace Multiplayer.Editor
         public Sprite refreshIcon;
         public Sprite connectIcon;
         public Sprite lanIcon;
-
-        public GameObject GetModelFromId(string id)
-        {
-            int index = 0;
-
-            while (index < playerPrefabs.Length)
-            {
-                var CharacterMetaData = playerPrefabs[index].GetComponent<CharacterMetaData>();
-                if (CharacterMetaData == null)
-                    Debug.LogError($"Player prefab at index {index} does not have CharacterMetaData component!");
-
-                if (CharacterMetaData.Id == id)
-                    return playerPrefabs[index];
-
-                index++;
-            }
-
-            Debug.LogWarning($"Could not find model with id {id}");
-
-            return null;
-        }
-
-        public IEnumerable<CharacterMetaData> AllCharacterMetaData()
-        {
-            int index = 0;
-
-            while (index < playerPrefabs.Length)
-            {
-                var metaData = playerPrefabs[index].GetComponent<CharacterMetaData>();
-
-                if (metaData == null)
-                    continue;
-
-                yield return metaData;
-
-                index++;
-            }
-        }
-        public IEnumerable<GameObject> AllCharacterModels() => playerPrefabs;
-
     }
 }
